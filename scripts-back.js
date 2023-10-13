31759da2861a8cdaab8472d857708c20aa8bf303
Juan Naula <j.naulas@jardinazuayo.fin.ec>
2023-10-13 00:33:33 -0500

    Ultima actualizacion se agrega tabla para presentar mejor la informacion y se agrega un respaldo de la version anterior

---
+++ scripts-back.js
@@ -0,0 +1,37 @@
+const API = 'https://swapi.dev/api/people/'
+
+function personaje(id,persona) {
+    let div = document.createElement('div')
+    let h1_texto = document.createTextNode(id + " "+persona.name)
+    let h1 = document.createElement('h1')
+    h1.appendChild(h1_texto)
+    div.appendChild(h1)
+
+    let contenedor = document.getElementById('contenedor')
+    contenedor.appendChild( div )
+}
+
+
+function obtener_personaje(id) {
+    return new Promise((resolve, reject) => {
+        fetch(`${API}${id}`)
+            .then((data) => resolve(data.json()))
+            .catch((error) => reject(`[error]: ${error}`))
+    })
+}
+
+let ids = []
+for (let i = 1; i < 80; i++) {
+    ids.push(i)
+}
+
+let promesas = ids.map(id => obtener_personaje(id))
+
+// Promesas Encadenadas
+Promise
+    .all(promesas)
+    .then(data => {
+        for (let i = 0; i < data.length; i++) {
+            personaje(i,data[i])
+        }
+    })
--- scripts.js
+++ scripts.js
@@ -1,15 +1,6 @@
 const API = 'https://swapi.dev/api/people/'
 
-function personaje(id,persona) {
-    let div = document.createElement('div')
-    let h1_texto = document.createTextNode(id + " "+persona.name)
-    let h1 = document.createElement('h1')
-    h1.appendChild(h1_texto)
-    div.appendChild(h1)
 
-    let contenedor = document.getElementById('contenedor')
-    contenedor.appendChild( div )
-}
 
 
 function obtener_personaje(id) {
@@ -24,14 +15,55 @@ let ids = []
 for (let i = 1; i < 80; i++) {
     ids.push(i)
 }
-
 let promesas = ids.map(id => obtener_personaje(id))
 
 // Promesas Encadenadas
 Promise
     .all(promesas)
     .then(data => {
+        const tableContainer = document.getElementById("table-container");
+        const table = document.createElement("table");
+        table.style.border = "1px solid black";
+        // Crear la cabecera de la tabla
+        const thead = document.createElement("thead");
+        const headerRow = document.createElement("tr");
+
+        // Definir las columnas de la cabecera
+        const headers = ["Codigo", "Nombre"];
+        // Crear las celdas de la cabecera
+        headers.forEach(headerText => {
+            const headerCell = document.createElement("th");
+            headerCell.textContent = headerText;
+            headerRow.appendChild(headerCell);
+            headerCell.style.border = "1px solid black";
+        });
+
+        // Agregar la fila de cabecera a la cabecera de la tabla
+        thead.appendChild(headerRow);
+
+        // Agregar la cabecera a la tabla
+        table.appendChild(thead);
+
         for (let i = 0; i < data.length; i++) {
-            personaje(i,data[i])
+            //personaje(i, data[i])
+            const dataRow = document.createElement("tr");
+
+            // Crear celdas de datos
+            const idCell = document.createElement("td");
+            idCell.textContent = i;
+            const nameCell = document.createElement("td");
+            nameCell.textContent = data[i].name;
+
+            // Agregar celdas de datos a la fila de datos
+            dataRow.appendChild(idCell);
+            dataRow.style.border = "1px solid black";
+            dataRow.appendChild(nameCell);
+            dataRow.style.border = "1px solid black";
+            // Agregar la fila de datos a la tabla
+            table.appendChild(dataRow);
+            
         }
+        // Agregar la tabla al contenedor
+        tableContainer.appendChild(table);
+
     })
