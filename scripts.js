e41ef768d1c08ad3e7d85b9816dc331c15886062
Juan Naula <j.naulas@jardinazuayo.fin.ec>
2023-10-12 23:40:02 -0500

    Update

--- scripts.js
+++ scripts.js
@@ -1,21 +1,19 @@
 const API = 'https://swapi.dev/api/people/'
 
-function personaje(persona) {
+function personaje(id,persona) {
     // Obtener una referencia al elemento contenedor de la tabla en el DOM
     const tableContainer = document.getElementById("table-container");
 
     // Crear una tabla
     const table = document.createElement("table");
 
-    // Agregar una clase CSS a la tabla si es necesario
-    table.classList.add("table-class"); // Reemplaza "table-class" por la clase CSS que desees.
-
     // Crear la cabecera de la tabla
     const thead = document.createElement("thead");
     const headerRow = document.createElement("tr");
 
     // Definir las columnas de la cabecera
-    const headers = ["Name", "Height", "Mass", "Gender", "Birth Year"];
+    //const headers = ["Codigo","Nombre", "Height", "Mass", "Gender", "Birth Year"];
+    const headers = ["Codigo","Nombre"];
 
     // Crear las celdas de la cabecera
     headers.forEach(headerText => {
@@ -32,27 +30,18 @@ function personaje(persona) {
     const dataRow = document.createElement("tr");
 
     // Crear celdas de datos
+    const idCell = document.createElement("td");
+    idCell.textContent = id;
+
     const nameCell = document.createElement("td");
     nameCell.textContent = persona.name;
 
-    const heightCell = document.createElement("td");
-    heightCell.textContent = persona.height;
-
-    const massCell = document.createElement("td");
-    massCell.textContent = persona.mass;
-
-    const genderCell = document.createElement("td");
-    genderCell.textContent = persona.gender;
-
-    const birthYearCell = document.createElement("td");
-    birthYearCell.textContent = persona.birth_year;
+  
 
     // Agregar celdas de datos a la fila de datos
+    dataRow.appendChild(idCell);
     dataRow.appendChild(nameCell);
-    dataRow.appendChild(heightCell);
-    dataRow.appendChild(massCell);
-    dataRow.appendChild(genderCell);
-    dataRow.appendChild(birthYearCell);
+   
 
     // Agregar la fila de datos a la tabla
     table.appendChild(dataRow);
@@ -79,6 +68,6 @@ Promise
     .all(promesas)
     .then(data => {
         for (let i = 0; i < data.length; i++) {
-            personaje(data[i])
+            personaje(i,data[i])
         }
     })
